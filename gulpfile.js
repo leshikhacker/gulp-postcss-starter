b76ee81c869c9e1e7060ca753dd9c86ca3ff00ae
leshikhacker <leshikhacker@gmail.com>
2015-08-25 12:24:58 +0300

    'init'

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var precss = require('precss');
+var mqpacker = require('css-mqpacker');
+var postcssImport  = require("postcss-import");
+
+
+gulp.task('serve', ['css'], function() {
+
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+
+  gulp.watch("./css/postcss/*.css", ['css']);
+  gulp.watch("./js/*.js").on('change', browserSync.reload);
+  gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+
+gulp.task('css', function () {
+  return gulp.src('./css/postcss/main.css')
+    .pipe(postcss([
+      postcssImport(),
+      precss,
+      autoprefixer({browsers: ['last 1 version']}),
+      mqpacker
+    ]))
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
